a848f2225292d6866516d55111cd7f3f452a212e
EnriqueLopez <llenriquelopez@gmail.com>
2019-04-27 19:48:15 -0500

    redirect

--- lib/pets_web/controllers/session_controller.ex
+++ lib/pets_web/controllers/session_controller.ex
@@ -24,7 +24,7 @@ defmodule PetsWeb.SessionController do
         username: login_user.username,
         email: login_user.email
       })
-      |> redirect(to: "/")
+      |> redirect(to: "/users/#{login_user.id}")
     else
       {:error, _} ->
         conn
