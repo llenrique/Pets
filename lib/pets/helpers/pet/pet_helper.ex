76b22ffc2976027db93b804499a184d3b6b87326
Enrique <llenriquelopez@gmail.com>
2019-05-18 15:27:20 -0500

    Add pet helpers

---
+++ lib/pets/helpers/pet/pet_helper.ex
@@ -0,0 +1,31 @@
+defmodule Pets.Helpers.PetHelper do
+  alias Pets.Contexts.PetManager
+
+  def new, do: {:ok, PetManager.new}
+
+  def create(user_id, attrs) do
+    attrs = Map.put(attrs, "user_id", user_id)
+    PetManager.create(attrs)
+  end
+
+  def show(id) do
+    {:ok, PetManager.list_single(id)}
+  end
+
+  def get_pet(id) do
+    PetManager.list_single(id)
+  end
+
+  def set_pet_changeset(pet) do
+    {:ok, PetManager.renew(pet)}
+  end
+
+  def update(id, params) do
+    pet = PetManager.list_single(id)
+    PetManager.update(pet, params)
+  end
+
+  def delete(id) do
+    PetManager.logical_delete(id)
+  end
+end
--- lib/pets_web/controllers/pet_controller.ex
+++ lib/pets_web/controllers/pet_controller.ex
@@ -1,22 +1,24 @@
 defmodule PetsWeb.PetController do
   use PetsWeb, :controller
-  alias Pets.Contexts.PetManager
+  alias Pets.Helpers.PetHelper
 
   @doc """
   Create a empty pet changeset and render a form to create a new pet
   """
 
   def new(conn, _params) do
-    pet = PetManager.new()
-    user = get_session(conn, :user)
+    case PetHelper.new do
+      {:ok, pet} ->
+        user = get_session(conn, :user)
 
-    conn
-    |> assign(:pet, pet)
-    |> assign(:user, user)
-    |> assign(:genders, GenderEnum.__enum_map__())
-    |> assign(:behaviors, BehaviorEnum.__enum_map__())
-    |> assign(:pet_types, PetTypeEnum.__enum_map__())
-    |> render("new.html")
+        conn
+        |> assign(:pet, pet)
+        |> assign(:user, user)
+        |> assign(:genders, GenderEnum.__enum_map__())
+        |> assign(:behaviors, BehaviorEnum.__enum_map__())
+        |> assign(:pet_types, PetTypeEnum.__enum_map__())
+        |> render("new.html")
+    end
   end
 
   @doc """
@@ -24,53 +26,57 @@ defmodule PetsWeb.PetController do
   """
   def create(conn, %{"pet" => attrs}) do
     user = get_session(conn, :user)
-    attrs = Map.put(attrs, "user_id", user.id)
-    {:ok, pet} = PetManager.create(attrs)
+    case PetHelper.create(user.id, attrs) do
+      {:ok, pet} ->
 
-    conn
-    |> put_flash(:info, "Pet Created")
-    |> redirect(to: Routes.user_pet_path(conn, :show, user.id, pet))
+        conn
+        |> put_flash(:info, "Pet Created")
+        |> redirect(to: Routes.user_pet_path(conn, :show, user.id, pet))
+    end
   end
 
   def show(conn, %{"id" => id}) do
-    pet = PetManager.list_single(id)
-    user = get_session(conn, :user)
+    case PetHelper.show(id) do
+      {:ok, pet} ->
+        user = get_session(conn, :user)
 
-    conn
-    |> assign(:user, user)
-    |> assign(:pet, pet)
-    |> render("show.html")
+        conn
+        |> assign(:user, user)
+        |> assign(:pet, pet)
+        |> render("show.html")
+    end
   end
 
   def edit(conn, %{"id" => id}) do
-    pet = PetManager.list_single(id)
-    changeset = PetManager.renew(pet)
-    user = get_session(conn, :user)
+    pet = PetHelper.get_pet(id)
+    case PetHelper.set_pet_changeset(pet) do
+      {:ok, changeset} ->
+        user = get_session(conn, :user)
 
-    conn
-    |> assign(:behaviors, BehaviorEnum.__enum_map__())
-    |> assign(:pet, pet)
-    |> assign(:u_session, user)
-    |> assign(:changeset, changeset)
-    |> render("edit.html")
+        conn
+        |> assign(:behaviors, BehaviorEnum.__enum_map__())
+        |> assign(:pet, pet)
+        |> assign(:u_session, user)
+        |> assign(:changeset, changeset)
+        |> render("edit.html")
+    end
   end
 
   def update(conn, %{"id" => id, "pet" => params}) do
-    pet = PetManager.list_single(id)
-    user = get_session(conn, :user)
-
-    case PetManager.update(pet, params) do
+    case PetHelper.update(id, params) do
       {:ok, u_pet} ->
+        user = get_session(conn, :user)
+
         conn
         |> put_flash(:info, "Pet updated")
         |> redirect(to: Routes.user_pet_path(conn, :show, user.id, u_pet.id))
     end
   end
 
-  def delete(conn, params) do
+  def delete(conn, %{"id" => id} = params) do
     user = get_session(conn, :user)
 
-    case PetManager.logical_delete(params["id"]) do
+    case PetHelper.delete(id) do
       {:ok, _} ->
         conn
         |> redirect(to: Routes.user_path(conn, :show, user.id))
